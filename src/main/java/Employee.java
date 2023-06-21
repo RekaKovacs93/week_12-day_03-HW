1413076ff5783f79ede13e910ed66284a0561876
Reka <reka.kovacs1993@gmail.com>
2023-06-21 17:51:31 +0100

    extension

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -21,9 +21,18 @@ public abstract class Employee {
         return name;
     }
     public double raiseSalary(double salaryRise){
-        return salary += salaryRise;
+        if(salaryRise >= 0){
+            salary += salaryRise;
+        }
+        return salary;
     }
     public double getBonus(){
         return this.salary / 100;
+    }
+
+    public void setName(String name) {
+        if (name != null) {
+            this.name = name;
+        }
     }
 }
